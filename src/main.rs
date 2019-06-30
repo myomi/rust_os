093a5cffb210ef424ba07c3c43f4ada804484c44
myomi <myomit@gmail.com>
2019-06-30 09:02:35 +0000

    Exit QEMU when tests finish.

--- src/main.rs
+++ src/main.rs
@@ -45,6 +45,8 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    // exit QEMU
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
@@ -52,4 +54,20 @@ fn trivial_assertion() {
     print!("trivial assertion... ");
     assert_eq!(1, 1);
     println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
